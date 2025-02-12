f0536b76dc6025382a190386a86f67cb829ebd94
cosmasobi <cosmasobi67@gmail.com>
2025-02-12 16:41:04 -0500

    made layout

---
+++ app/src/main/java/com/example/restaurantrater/Restaurant.java
@@ -0,0 +1,53 @@
+package com.example.restaurantrater;
+
+public class Restaurant {
+    private String restaurantName;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String zipcode;
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
---
+++ app/src/main/java/com/example/restaurantrater/RestaurantDBHelper.java
@@ -0,0 +1,4 @@
+package com.example.restaurantrater;
+
+public class RestaurantDBHelper {
+}
